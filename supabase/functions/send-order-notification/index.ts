954d33f8d3f16f8150a4efca6269e8ef2d665f17
MominMohammedImr <mominmohammedimran11@gmail.com>
2025-06-22 00:30:29 +0530

    jjhk

--- supabase/functions/send-order-notification/index.ts
+++ supabase/functions/send-order-notification/index.ts
@@ -8,7 +8,7 @@ import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 
 const corsHeaders = {
 
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": "https://b3f-prints.pages.dev",
 
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
 
